1cbbcb2b01190878dc08df36617dd059cf882444
Heba-Webdev23 <jtvonline@yahoo.com>
2023-01-11 02:09:02 +0300

    feat navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './Components/Header'
 function App() {
   return (
-    <div className="">
+    <div className=" ">
       <Header />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+     container: {
+      padding: '2rem',
+    },
     extend: {
       backgroundImage: {
         
